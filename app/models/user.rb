70e998c060cf9996653d96a49d4918a44fd3eea6
Giorgia Willits <gwillits@berkeley.edu>
2016-11-12 06:27:55 +0000

    log in users but can't log out

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# app/models/user.rb
+
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :email, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,
+    message: ": an email is incorrectly formatted" }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def full_name
+    self.first_name.capitalize + " " + self.last_name.capitalize
+  end
+
+  def first_name_initial
+    self.first_name.capitalize + " " + self.last_name.capitalize[0] + "."
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
+  
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +55,10 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  get '/signup' => 'users#new'
+  # post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end

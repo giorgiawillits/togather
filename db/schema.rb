b949fdb5daac17cd07c56ef2a581c2cffe8b9d55
lcapelluto <lcapelluto@berkeley.edu>
2016-11-12 21:03:32 +0000

    about to merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112061322) do
+ActiveRecord::Schema.define(version: 20161112065127) do
+
+  create_table "networks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "networks", ["user_id"], name: "index_networks_on_user_id"
+
+  create_table "networks_users", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "network_id"
+  end
+
+  add_index "networks_users", ["network_id"], name: "index_networks_users_on_network_id"
+  add_index "networks_users", ["user_id"], name: "index_networks_users_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"

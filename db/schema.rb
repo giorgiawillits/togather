9f63f68b8d380ec708baa0811b1a7907afe769c5
Pauline Duprat <paulineduprat@berkeley.edu>
2016-11-12 22:21:57 +0000

    login page front end

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112065127) do
+ActiveRecord::Schema.define(version: 20161112203000) do
+
+  create_table "groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups_users", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+  end
+
+  add_index "groups_users", ["group_id"], name: "index_groups_users_on_group_id"
+  add_index "groups_users", ["user_id"], name: "index_groups_users_on_user_id"
 
   create_table "networks", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,7 +41,6 @@ ActiveRecord::Schema.define(version: 20161112065127) do
 
   add_index "networks_users", ["network_id"], name: "index_networks_users_on_network_id"
   add_index "networks_users", ["user_id"], name: "index_networks_users_on_user_id"
-
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"

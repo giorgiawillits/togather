202b20f5ce94e834b8999d05b35500c564080e84
Valeriya <vimesheva@berkeley.edu>
2016-11-12 22:08:39 +0000

    seeds up to date

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112203000) do
+ActiveRecord::Schema.define(version: 20161112210800) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string   "title"
+    t.string   "description"
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "events", ["group_id"], name: "index_events_on_group_id"
+  add_index "events", ["user_id"], name: "index_events_on_user_id"
 
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "groups_posts", id: false, force: :cascade do |t|
@@ -50,8 +65,6 @@ ActiveRecord::Schema.define(version: 20161112203000) do
   add_index "networks_users", ["network_id"], name: "index_networks_users_on_network_id"
   add_index "networks_users", ["user_id"], name: "index_networks_users_on_user_id"
 
-<<<<<<< HEAD
-=======
   create_table "posts", force: :cascade do |t|
     t.string   "description"
     t.integer  "user_id"
@@ -61,7 +74,6 @@ ActiveRecord::Schema.define(version: 20161112203000) do
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
->>>>>>> 09c0019f4a3275f476df82b91b2860ba51a60312
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
